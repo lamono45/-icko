b61a579165b3d53a8b47e0ebf848053b579b200b
lamono45 <abanglamono@gmail.com>
2025-07-10 20:15:13 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -533,7 +533,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit Quota"
-wget raw.githubusercontent.com/arifkenza09/vip/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
+wget raw.githubusercontent.com/lamono45/vip/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
 
 cd
 wget -q -O /usr/bin/limit-ip "${REPO}limit/limit-ip"
